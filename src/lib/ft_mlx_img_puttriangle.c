c6e4dd0ed549abc3e402c8738c3efaf55e091099
um4ss <sliziard@icloud.com>
2025-09-27 14:00:44 +0200

    fix(ft_mlx_img_put_triangle): increase triangle precision but more changing
    determine the 'box' of the triangle and iterate on each pos from min to max y of it
    for each pos we use _edge_fn and top left rule to determine if we print it or not
    that increase sharpness of triangles edges

--- src/lib/ft_mlx_img_puttriangle.c
+++ src/lib/ft_mlx_img_puttriangle.c
@@ -6,111 +6,103 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 16:32:45 by sliziard          #+#    #+#             */
-/*   Updated: 2025/09/26 14:08:38 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/09/27 13:53:23 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "ft_mlx_img.h"
 #include "color.h"
+#include "vec/ftmath_utils.h"
 #include "vec/vec.h"
 
-/* sort in ascending order
-	v.a = top, v.b = middle, v.c = bottom 
-*/
-static void	sort_by_y(t_vec2iv3 *v)
+#define AB	0
+#define BC	1
+#define CA	2
+
+static int64_t	_edge_fn(t_vec2i lvertice, t_vec2i rvertice, t_vec2i curr_p)
 {
-	if (v->a.y > v->b.y)
-		vec2i_swap(&v->a, &v->b);
-	if (v->a.y > v->c.y)
-		vec2i_swap(&v->a, &v->c);
-	if (v->b.y > v->c.y)
-		vec2i_swap(&v->b, &v->c);
+	int64_t	v;
+	int64_t	wx;
+	int64_t	wy;
+
+	wx = (int64_t)(curr_p.x - lvertice.x);
+	wy = (int64_t)(curr_p.y - lvertice.y);
+	v = wx * (int64_t)(rvertice.y - lvertice.y)
+		- wy * (int64_t)(rvertice.x - lvertice.x);
+	return (v);
 }
 
-/* tri.a.y == tri.b.y : flat bottom, tri.c to top */
-static void	_fill_flat_bottom(t_img *img, t_vec2iv3 tri, t_color color)
+static inline bool	_is_top_left(t_vec2i a, t_vec2i b)
 {
-	double dx0;
-	double dx1;
-	t_vec2d	left;
-	t_vec2d	right;
+	t_vec2i	delta;
 
-	dx0 = (double)(tri.c.x - tri.a.x) / (double)(tri.c.y - tri.a.y);
-	dx1 = (double)(tri.c.x - tri.b.x) / (double)(tri.c.y - tri.b.y);
-	left = (t_vec2d){tri.a.x, tri.a.y};
-	right = (t_vec2d){tri.b.x, tri.b.y};
-	while (left.y <= tri.c.y)
-	{
-		ft_mlx_img_put_span(img,
-			(t_vec2iv2){
-				vec2d_round(left, false),
-				vec2d_round(right, false)},
-			color);
-		left = vec2d_sum(left, (t_vec2d){dx0, 1});
-		right = vec2d_sum(right, (t_vec2d){dx1, 1});
-	}
+	delta = vec2i_minus(b, a);
+	return ((delta.y > 0) || (delta.y == 0 && delta.x < 0));
 }
 
-/* tri.b.y == tri.c.y : flat bottom, tri.a to top */
-static void	_fill_flat_top(t_img *img, t_vec2iv3 tri, t_color color)
+static void	_bbox_from_tri(t_vec2iv3 t, t_vec2i img, t_vec2i *mn, t_vec2i *mx)
 {
-	double dx0;
-	double dx1;
-	t_vec2d	left;
-	t_vec2d	right;
+	t_vec2i	minv;
+	t_vec2i	maxv;
 
-	dx0 = (double)(tri.b.x - tri.a.x) / (double)(tri.b.y - tri.a.y);
-	dx1 = (double)(tri.c.x - tri.a.x) / (double)(tri.c.y - tri.a.y);
-	left = (t_vec2d){tri.a.x, tri.a.y};
-	right = left;
-	while (left.y < tri.b.y)
-	{
-		ft_mlx_img_put_span(img,
-			(t_vec2iv2){
-				vec2d_round(left, true),
-				vec2d_round(right, true)},
-			color);
-		left = vec2d_sum(left, (t_vec2d){dx0, 1});
-		right = vec2d_sum(right, (t_vec2d){dx1, 1});
-	}
+	minv.x = ftm_min3(t.a.x, t.b.x, t.c.x);
+	minv.y = ftm_min3(t.a.y, t.b.y, t.c.y);
+	maxv.x = ftm_max3(t.a.x, t.b.x, t.c.x);
+	maxv.y = ftm_max3(t.a.y, t.b.y, t.c.y);
+	if (minv.x < 0)
+		minv.x = 0;
+	if (minv.y < 0)
+		minv.y = 0;
+	if (maxv.x >= img.x)
+		maxv.x = img.x - 1;
+	if (maxv.y >= img.y)
+		maxv.y = img.y - 1;
+	*mn = minv;
+	*mx = maxv;
 }
 
-static t_vec2i	interp_at_y(t_vec2i p0, t_vec2i p1, int y)
+static inline bool	_is_px_printable(t_vec2iv3 tri, t_vec2i curr_p, uint8_t tl)
 {
-	double	t;
-	t_vec2i	i;
-	int32_t	dy;
+	t_vec3l	e;
 
-	dy = (p1.y - p0.y);
-	if (dy == 0)
-		return ((t_vec2i){ p0.x, y });
-	t = (double)(y - p0.y) / (double)dy;
-	i.x = p0.x + (int)((p1.x - p0.x) * t);
-	i.y = y;
-	return (i);
+	e.x = _edge_fn(tri.a, tri.b, curr_p);
+	e.y = _edge_fn(tri.b, tri.c, curr_p);
+	e.z = _edge_fn(tri.c, tri.a, curr_p);
+	return (
+		(e.x > 0 || (!e.x && (tl >> AB) & 1))
+		&& (e.y > 0 || (!e.y && (tl >> BC) & 1))
+		&& (e.z > 0 || (!e.z && (tl >> CA) & 1))
+	);
 }
 
-void	ft_mlx_img_put_triangle(t_img *img, t_vec2iv3 triangle, t_color color)
+void	ft_mlx_img_put_triangle(t_img *img, t_vec2iv3 tri, t_color color)
 {
-	t_vec2iv3	v;
-	t_vec2i		split;
+	t_vec2i	min;
+	t_vec2i	max;
+	t_vec2i	p;
+	uint8_t	top_left;
+	int64_t	area;
 
-	v = triangle;
-	sort_by_y(&v);
-	if (v.a.y == v.c.y)
-	{
-		ft_mlx_img_put_span(img, (t_vec2iv2){v.a, v.b}, color);
-		ft_mlx_img_put_span(img, (t_vec2iv2){v.a, v.c}, color);
+	area = _edge_fn(tri.a, tri.b, tri.c);
+	if (!area)
 		return ;
+	(void)(area < 0 && (vec2i_swap(&tri.b, &tri.c), 1));
+	_bbox_from_tri(tri, img->dim, &min, &max);
+	top_left = (_is_top_left(tri.a, tri.b) | (_is_top_left(tri.b, tri.c) << 1)
+			| (_is_top_left(tri.c, tri.a) << 2));
+	p.y = min.y;
+	while (p.y <= max.y)
+	{
+		p.x = min.x;
+		while (p.x <= max.x)
+		{
+			if (_is_px_printable(tri, p, top_left))
+				ft_mlx_img_put_px(img, p, color.value);
+			p.x++;
+		}
+		p.y++;
 	}
-	if (v.b.y == v.a.y)
-		return (_fill_flat_bottom(img, v, color));
-	if (v.b.y == v.c.y)
-		return (_fill_flat_top(img, v, color));
-	split = interp_at_y(v.a, v.c, v.b.y);
-	_fill_flat_top(img, (t_vec2iv3){v.a, v.b, split}, color);
-	_fill_flat_bottom(img, (t_vec2iv3){v.b, split, v.c}, color);
 }
-
