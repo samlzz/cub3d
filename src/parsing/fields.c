a7d4283095b9b86b925ed4284ee949451fbf7840
um4ss <sliziard@icloud.com>
2025-09-29 16:43:33 +0200

    fix(fields): correct order of WE and EA

--- src/parsing/fields.c
+++ src/parsing/fields.c
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 21:50:00 by sliziard          #+#    #+#             */
-/*   Updated: 2025/09/29 12:50:36 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/09/29 16:23:59 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ t_field_meta	fields_get_metadata(t_field_id id)
 	static const t_field_meta	_fields_meta[FI__COUNT] = {
 	{ "NO", FK_TEXTURE, true, "North texture (XPM file)." },
 	{ "SO", FK_TEXTURE, true, "South texture (XPM file)." },
-	{ "WE", FK_TEXTURE, true, "West texture (XPM file)." },
 	{ "EA", FK_TEXTURE, true, "East texture (XPM file)." },
+	{ "WE", FK_TEXTURE, true, "West texture (XPM file)." },
 	{ "F", FK_COLOR, true, "Floor color: R,G,B (0..255)." },
 	{ "C", FK_COLOR, true, "Ceiling color: R,G,B (0..255)." },
 	{ "MAP", FK_REST, true, "Grid content until EOF."},
@@ -38,10 +38,10 @@ void	*fields_get_dst(t_map *m, t_field_id id)
 		return (void *)&m->textures[TEX_N].path;
 	if (id == FI_SO)
 		return (void *)&m->textures[TEX_S].path;
-	if (id == FI_WE)
-		return (void *)&m->textures[TEX_W].path;
 	if (id == FI_EA)
 		return (void *)&m->textures[TEX_E].path;
+	if (id == FI_WE)
+		return (void *)&m->textures[TEX_W].path;
 	if (id == FI_FLOOR)
 		return (void *)&m->floor_colr;
 	if (id == FI_CEIL)
