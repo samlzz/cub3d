348b5527ee2a4741cca10ae05d9f5e7ee5967456
um4ss <sliziard@icloud.com>
2025-09-29 08:55:38 +0200

    feat(ft_mlx_textures): add load and destroy funcs

--- src/lib/ft_mlx/ft_mlx_texture.c
+++ src/lib/ft_mlx/ft_mlx_texture.c
@@ -1,1 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mlx_texture.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/28 18:54:21 by sliziard          #+#    #+#             */
+/*   Updated: 2025/09/28 19:36:11 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "mlx.h"
+#include "data/data.h"
+#include "parsing/parse_err.h"
 #include "ft_mlx_texture.h"
+
+static inline bool	_file_found_and_readable_(char *path)
+{
+	int	fd;
+
+	if (!path)
+		return (1);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (open_err(path), false);
+	return (close(fd), true);
+}
+
+int16_t	ft_mlx_texture_load(t_texture *tex, t_mlx *mlx)
+{
+	t_img	img;
+
+	if (!tex)
+		return (2);
+	if (!_file_found_and_readable_(tex->path))
+		return (1);
+	img.image_ptr = mlx_xpm_file_to_image(
+		mlx->display,
+		tex->path,
+		&img.dim.x, &img.dim.y);
+	if (!img.image_ptr)
+		return (1);
+	img.data_addr = mlx_get_data_addr(
+		img.image_ptr,
+		&img.bpp, &img.line_len, &img.endian
+	);
+	tex->img = img;
+	return (0);
+}
+
+void	ft_mlx_texture_destroy(t_texture *tex, t_mlx *mlx)
+{
+	free(tex->path);
+	if (tex->img.data_addr)
+		mlx_destroy_image(mlx->display, tex->img.image_ptr);
+}
+
+int16_t	ft_mlx_textures_load_list(t_texture *list, int32_t count, t_mlx *mlx)
+{
+	int32_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		if (ft_mlx_texture_load(list + i, mlx))
+		{
+			while (--i)
+				ft_mlx_texture_destroy(list + i, mlx);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
--- src/lib/ft_mlx/ft_mlx_texture.h
+++ src/lib/ft_mlx/ft_mlx_texture.h
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 21:12:45 by sliziard          #+#    #+#             */
-/*   Updated: 2025/09/27 21:16:32 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/09/28 19:18:15 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@
 
 #include "ft_mlx_img.h"
 
+typedef struct s_mlx	t_mlx;
+
 typedef struct s_texture	t_texture;
 
 struct s_texture
 {
 	char		*path;
-	t_img		*img;
+	t_img		img;
 };
 
-int16_t	ft_mlx_texture_load(t_texture *tex);
-void	ft_mlx_texture_destroy(t_texture *tex);
+int16_t	ft_mlx_texture_load(t_texture *tex, t_mlx *mlx);
+int16_t	ft_mlx_textures_load_list(t_texture *list, int32_t count, t_mlx *mlx);
+void	ft_mlx_texture_destroy(t_texture *tex, t_mlx *mlx);
 
 #endif
